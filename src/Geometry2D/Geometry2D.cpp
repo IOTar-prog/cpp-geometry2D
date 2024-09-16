a7d19d753c8fbf907b1c47abc365cb9f5ed4c801
IOTar-prog <70750411+IOTar-prog@users.noreply.github.com>
2024-09-16 22:24:45 +0300

    1.1-1.4

--- src/Geometry2D/Geometry2D.cpp
+++ src/Geometry2D/Geometry2D.cpp
@@ -1,20 +1,59 @@
-﻿// Geometry2D.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿
+#include <fstream> 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+struct ConsoleConfig {
+    string geometryFileFormat;
+    string geometryFilePath;
+};
+
+struct GeometreDescription {
+    int geometryFragmentSizeX;
+    int geometryFragmentSizeY;
+    int geometryFragmentNumX;
+    int geometryFragmentNumY;
+    bool* data;
+};
+
+void ReadCommandLineArgs(int argc, char* argv[], ConsoleConfig& ConsConf) {
+    for (int i = 0; i < argc; i++) {
+        if (i + 1 < argc) {
+            if (string(argv[i]) == "-ff") {
+                ConsConf.geometryFileFormat = string(argv[i + 1]);
+            }
+            if (string(argv[i]) == "-fp") {
+                ConsConf.geometryFilePath = string(argv[i + 1]);
+            }
+        }
+    }
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+int main(int argc, char* argv[])
+{   
+    
+    ConsoleConfig ConsConf;
+    GeometreDescription GeomDesc;
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+    ReadCommandLineArgs(argc, argv, ConsConf);
+
+    ifstream file("C:/Users/ilyat/Desktop/cpp geometry/cpp-geometry2D/data/txt.txt");
+    if (file.is_open()) {
+        file >> GeomDesc.geometryFragmentSizeX;
+        file >> GeomDesc.geometryFragmentSizeY;
+        file >> GeomDesc.geometryFragmentNumX;
+        file >> GeomDesc.geometryFragmentNumY;
+        file.close();
+
+        cout << GeomDesc.geometryFragmentSizeX;
+        cout << GeomDesc.geometryFragmentSizeY;
+        cout << GeomDesc.geometryFragmentNumX;
+        cout << GeomDesc.geometryFragmentNumY;
+    }
+    else cout << "Error \n";
+
+    //GeomDesc.data = new bool[GeomDesc.geometryFragmentNumX * GeomDesc.geometryFragmentNumY];
+ 
+}
+
