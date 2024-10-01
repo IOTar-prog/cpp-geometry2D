3929301c7774e0344606dad1dbf412feedba596e
IOTar-prog <70750411+IOTar-prog@users.noreply.github.com>
2024-10-01 12:27:50 +0300

    Init a .h file

---
+++ src/Geometry2D/Header1.h
@@ -0,0 +1,173 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class CPU {
+private:
+	unsigned int freq;
+	unsigned short thread_count;
+public:
+
+	void SetFreq(unsigned short Hz) {
+		freq = Hz;
+	}
+
+	void SetThread_count(unsigned short num) {
+		thread_num = num;
+	}
+
+	unsigned int TakeFreq() {
+		return freq;
+	}
+
+	unsigned short TakeThread_count() {
+		return thread_count;
+	}
+
+	void ShowSpecs() {
+		cout << " frequencies = " << freq << "\n";
+		cout << " CPU have a " << thread_count << "threads" << "\n";
+	}
+
+};
+
+class GPU {
+private:
+	unsigned int amount_of_memory;
+	unsigned int сore_freq;
+	unsigned int memory_freq;
+	unsigned short memory_bus_width;
+public:
+
+	void SetCoreFreq(unsigned int Hz) {
+		core_freq = Hz;
+	}
+
+	void SetMemFreq(unsigned int Hz) {
+		memory_freq = Hz;
+	}
+
+	void SetAmOfMem(unsigned int Mb) {
+		amount_of_memory = Mb;
+	}
+
+	void SetMemBusWid(unsigned int Bit) {
+		memory_bus_width = Bit;
+	}
+
+	unsigned int TakeCoreFreq() {
+		return core_freq;
+	}
+
+	unsigned int TakeMemFreq() {
+		return memory_freq;
+	}
+
+	unsigned int TakeAmOfMem() {
+		return amount_of_memory;
+	}
+
+	unsigned short TakeMemBusWid() {
+		return memory_bus_width;
+	}
+
+	void ShowSpecs() {
+		cout << " Amount of memory: " << amount_of_memory << "Mb" << "\n";
+		cout << " Core frequency: " << сore_freq / 1000 << "MHz" << "\n";
+		cout << " Memory frequency: " << memory_freq / 1000 << "MHz" << "\n";
+		cout << " Memory bus width: " << memory_bus_width << "Bit" << "\n";
+	}
+}
+
+class Ram {
+public:
+	string type;
+	unsigned int freq;
+	unsigned int amount_of_memory;
+private:
+
+	void SetType(string DDR_type) {
+		type = DDR_type;
+	}
+
+	void SetFreq(unsigned int HZ) {
+		freq = Hz;
+	}
+
+	void AmOfMem(unsigned int Mb) {
+		amount_of_memory = Mb;
+	}
+
+	string TakeType() {
+		return type;
+	}
+
+	unsigned int TakeFreq() {
+		return freq;
+	}
+
+	unsigned int TakeAmOfMem() {
+		return amount_of_memory;
+	}
+
+	void SgowSpecs() {
+		cout << " Amount of memory: " << amount_of_memory << "Mb" << "\n";
+		cout << " Frequency: " << freq / 1000 << "MHz" << "\n";
+		cout << " Type: " << type << "\n";
+	}
+};
+
+class Node {
+private:
+	vector<CPU> CPU_spec;
+	vector<GPU> GPU_spec;
+	vector<Ram> RAM_spec;
+public:
+
+	void SetCpuSpecs(const CPU& CPU_object) {
+		CPU_spec.push_back(CPU_object)
+	}
+
+	void SetGpuSpecs(const GPU& GPU_object) {
+		GPU_spec.push_back(GPU_object)
+	}
+
+	void SetRamSpecs(const RAM& RAM_object) {
+		RAM_spec.push_back(RAM_object)
+	}
+
+	// количество элементов в соответствующем векторе
+	unsigned short CountOfElem(unsigned short id_of_elem) {
+		switch (id_of_elem) {
+		case 1:
+			return unsigned short(CPU_spec.size());
+			break;
+		case 2:
+			return unsigned short(GPU_spec.size());
+			break;
+		case 3:
+			return unsigned short(RAM_spec.size());
+			break;
+		default:
+			cout << "Node:CountOfElem error. Wrong id of elem!";
+			break;
+		}
+	}
+
+	// как вернуть адреса нескольких элементов?
+	CPU& TakeCpu() {
+
+	}
+
+	GPU& TakeGpu() {
+
+	}
+
+	RAM& TakeRam() {
+
+	}
+
+};
